44ea7ae6750a8f81693fe42d05a6176a77b1d7d7
Eze Samuel <ifeanyialex2@gmail.com>
2023-01-24 11:51:59 -0800

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
 export default function getResponseFromAPI() {
-  const promise = new Promise((resolve, reject) => {
-    const error = false;
-    if (error) {
-      resolve('Hello resolved');
-    } else reject(new Error('something bad happened'));
-  });
-  return promise;
+  return new Promise((resolve, reject) => [resolve, reject]);
 }
--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,7 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else reject(new Error({}));
-  });
+export default function handleResponseFromAPI(promise) {
+  const obj = { status: 200, body: 'success' };
+  return promise
+    .then(() => obj)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
-getFullResponseFromAPI()
-  .then(console.log('Got a response from the API'))
-  .catch((e) => {
-    console.log(e); // works!
-  });
